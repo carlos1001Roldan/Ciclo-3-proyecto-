6816464915f99e5875f843ae466d1d8a351aa616
coscristian <crisquesadaco@gmail.com>
2022-09-28 09:35:16 -0500

    Creación Data Access Objects

---
+++ src/main/java/com/proyecto/ciclo3/proyectocursos/controller/dto/EspecialidadDto.java
@@ -0,0 +1,14 @@
+package com.proyecto.ciclo3.proyectocursos.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EspecialidadDto {
+    private String nombre;
+    private String descripcion;
+    private Integer id;
+}
